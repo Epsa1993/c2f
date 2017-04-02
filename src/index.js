1026f028fa2f119f9b1d3045140ff45522bad1a5
aman.kumar <aman.kumar@jaarvis.com>
2017-04-02 10:51:27 +0530

    c2f

--- src/index.js
+++ src/index.js
@@ -1,2 +1,18 @@
-/* eslint-disable no-console */
-console.log('hi');
+import 'babel-polyfill';
+import React from 'react';
+import { render } from 'react-dom';
+import { Router, browserHistory } from 'react-router';
+import routes from './routes';
+// import './styles/styles.css';
+import {provider} from 'react-redux';
+import configureStore from './store/configureStore';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+const store= configureStore();
+
+render(
+	<provider store={store}>
+		<Router history={browserHistory} routes={routes} />
+	</provider>,
+  document.getElementById('app')
+);
---
+++ src/routes.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Route, IndexRoute } from 'react-router';
+// import App from './components/App';
+import Login from './component/account/Login';
+
+
+export default (
+  <Route path="/login" component={Login}>
+   </Route>
+);
